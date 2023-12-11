c1451502332475c5684642ede654c08baf24de05
khamza <khamza.khudayberdiyev@gmail.com>
2023-12-11 16:00:25 +0500

    update

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return redirect()->route('home');
+//});
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\IndexController::class, 'index'])->name('index');
